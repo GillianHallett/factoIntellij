7dd250ac068262bf2d8c3a410d958b8da1f54549
GillianHallett <chalethallett303@gmail.com>
2023-03-21 10:05:52 +0000

    comit inicial en intellij

---
+++ src/NumeroFactorial.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+/*
+ Clase que calcula el factorial de un numero.
+ */
+public class NumeroFactorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fac = 1, num, exit = 0;
+
+        do {
+            System.out.println("Introduce un numero: ");
+            num = sc.nextInt();
+            if (num > 0) {
+                System.out.println("El factor de " + num + " es " + factorial(num));
+            }
+        } while (exit != 0);
+        System.out.println("si desea salir del programa pulse 0");
+        exit = sc.nextInt();
+    }
+
+    private static int factorial(int num) {
+        if ( num == 0)
+            return 1;
+        else
+            return num*factorial(num - 1);
+
+    }
+
+}
